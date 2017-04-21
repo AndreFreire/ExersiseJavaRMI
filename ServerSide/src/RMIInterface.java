9beb24b992057f452ea871147f5a803561836802
Andre Freire <and__re@hotmail.com>
2017-04-21 13:40:01 -0300

    partmodel and interface

--- ServerSide/src/RMIInterface.java
+++ ServerSide/src/RMIInterface.java
@@ -1,8 +1,14 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface RMIInterface extends Remote {
-
     public String helloTo(String name) throws RemoteException;
-
+    public String savePart(PartModel part) throws RemoteException;
+    public String getPart(int id) throws RemoteException;
+    public String deletePart(int id) throws RemoteException;
+    public ArrayList<PartModel> listParts() throws RemoteException;
+    public ArrayList<PartModel> listSubPartsById(int id) throws RemoteException;
+    public String cleanSubParts(int id) throws RemoteException;
+    public String addSubPart(PartModel part) throws RemoteException;
 }
--- ServerSide/src/ServerOperation.java
+++ ServerSide/src/ServerOperation.java
@@ -6,24 +6,20 @@ import java.util.ArrayList;
 public class ServerOperation extends UnicastRemoteObject implements RMIInterface{
 
     private static final long serialVersionUID = 1L;
-    ArrayList teste = new ArrayList<String>();
+    ArrayList<PartModel> teste = new ArrayList<PartModel>();
 
     protected ServerOperation() throws RemoteException {
-
         super();
-
     }
 
     @Override
     public String helloTo(String name) throws RemoteException{
-    	teste.add(name);
-    	for(int i=0;i<teste.size();i++){System.err.println(teste.get(i));}
-        System.err.println(name + " is trying to contact!");
+    	
+    	System.err.println(name + " is trying to contact!");
         
         return "Server says hello to " + name;
 
     }
-
     public static void main(String[] args){
 
         try {
